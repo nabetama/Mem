5e9052ed3e58b3f7d3c25b6fc3c318a4b8015b68
nabetama <mao.nabeta@gmail.com>
2015-06-25 17:39:54 +0900

    memoize decorator.

---
+++ memoize/memoize.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+""" Cached decorater MixIn.
+"""
+class Mem(object):
+    @staticmethod
+    def memoize(f):
+        def _cache(*args, **kwargs):
+            if not hasattr(f, 'cache'):
+                f.cache = { }
+            result = f.cache.get(f.__name__)
+            if result:
+                return result
+            result = f(*args, **kwargs)
+            f.cache[f.__name__] = result
+            return result
+        return _cache
